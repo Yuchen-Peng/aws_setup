dc34d8d38898f31e9a2e3fc40e3521f4a0217ee4
peng19880925 <pengpyc@gmail.com>
2017-11-08 11:43:53 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+sudo apt-get install wget
+sudo apt-get install bzip2
+sudo apt-get install nano
+sudo apt-get install mesa-libGL.x86_64 # for matplotlib.pyplot
+
+sudo apt-get upgrade
+
+#anaconda
+#check https://repo.continuum.io/archive/index.html
+wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
+
+bash Anaconda3-4.3.0-Linux-x86_64.sh
+
+source ~/.bashrc
+
