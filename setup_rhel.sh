72cda66aaf29496333993f5087d6cf4daa95fe78
peng19880925 <pengpyc@gmail.com>
2017-11-08 11:50:45 -0500

    Create setup_rhel.sh

---
+++ setup_rhel.sh
@@ -0,0 +1,18 @@
+sudo yum install wget
+sudo yum install bzip2
+sudo yum install nano
+sudo yum install mesa-libGL.x86_64 # for matplotlib.pyplot
+
+sudo yum upgrade
+
+#anaconda
+#check https://repo.continuum.io/archive/index.html
+wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
+
+bash Anaconda3-4.3.0-Linux-x86_64.sh
+
+source ~/.bashrc
+
+sudo yum install python-pip python-dev build-essential 
+sudo pip install --upgrade pip 
+sudo pip install --upgrade virtualenv 
