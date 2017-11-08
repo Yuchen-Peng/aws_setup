638a5a157eb124c5ce2f09b654ff2a7a02893e13
peng19880925 <pengpyc@gmail.com>
2017-11-08 11:47:21 -0500

    Create model_setup.sh

---
+++ model_setup.sh
@@ -0,0 +1,10 @@
+# install the necessary packages for modeling; including xgboost, sklearn, matplotlib, seaborn, tensorflow, and pymc3
+
+pip install scikit-learn --upgrade
+conda install matplotlib
+pip install seaborn --upgrade
+
+pip install http://h2o-release.s3.amazonaws.com/h2o/rel-ueno/1/Python/h2o-3.10.4.1-py2.py3-none-any.whl
+pip install h2o --upgrade
+python3 -m pip install tensorflow # tensorflow
+pip install pymc3 --upgrade #py bayesian
